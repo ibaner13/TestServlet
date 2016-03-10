9faed6c30d1938e8e92e2393618dc5c1ea0da8b7
Ishika Banerjee <ishikabanerjee100@gmail.com>
2016-03-10 09:45:54 +0530

    Test Servlet Code

---
+++ TestServlet/src/com/training/servlets/MyServlet.java
@@ -0,0 +1,49 @@
+package com.training.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("txt1");
+		PrintWriter out = response.getWriter();
+		out.println("Hello " + name);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("txt1");
+		PrintWriter out = response.getWriter();
+		out.println("Hello " + name);
+	}
+
+}
